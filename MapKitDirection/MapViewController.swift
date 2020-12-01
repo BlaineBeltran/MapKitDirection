398da3e09aaa058307f0ae0f1d39aeac747c0527
blainebeltran <blaine.abeltran@gmail.com>
2020-12-01 17:18:25 -0600

    Initial Commit

---
+++ MapKitDirection/MapViewController.swift
@@ -0,0 +1,171 @@
+//
+//  MapViewController.swift
+//  MapKitDirection
+//
+//  Created by Simon Ng on 6/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    // Method to show direction when the location button is pressed
+    @IBAction func showDirection(sender: UIButton) {
+        guard let currentPlacemark = currentPlacemark else {
+            return
+        }
+        
+        let directionRequest = MKDirections.Request()
+        
+        // Set the source and destination of the route
+        directionRequest.source = MKMapItem.forCurrentLocation()
+        let destinationPlacemark = MKPlacemark(placemark: currentPlacemark)
+        directionRequest.destination = MKMapItem(placemark: destinationPlacemark)
+        directionRequest.transportType = MKDirectionsTransportType.automobile
+        
+        // Calculate the direction
+        let directions = MKDirections(request: directionRequest)
+        
+        directions.calculate { (routeResponse, routeError) -> Void in
+            
+            guard let routeResponse = routeResponse else {
+                if let routeError = routeError {
+                    print("Error: \(routeError)")
+                }
+                
+                return
+            }
+            
+            let route = routeResponse.routes[0]
+            self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.aboveRoads)
+            
+            // Scale to fit the directions in the smallest rectangle (show all of the directions)
+            let rect = route.polyline.boundingMapRect
+            self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+        }
+    }
+    
+    // Method to draw the route
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 3.0
+        
+        return renderer
+    }
+    
+    let locationManager = CLLocationManager()
+    
+
+    @IBOutlet var mapView: MKMapView!
+    
+    var currentPlacemark: CLPlacemark?
+    
+    var restaurant:Restaurant!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Request for a user's authorization for location services
+        locationManager.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            
+            // Show user location
+            mapView.showsUserLocation = true
+        }
+
+        // Convert address to coordinate and annotate it on map
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(restaurant.location, completionHandler: { placemarks, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            if let placemarks = placemarks {
+                // Get the first placemark
+                let placemark = placemarks[0]
+                self.currentPlacemark = placemark
+                
+                // Add annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = self.restaurant.name
+                annotation.subtitle = self.restaurant.type
+                
+                if let location = placemark.location {
+                    annotation.coordinate = location.coordinate
+                    
+                    // Display the annotation
+                    self.mapView.showAnnotations([annotation], animated: true)
+                    self.mapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+            
+        })
+        
+        mapView.delegate = self
+        if #available(iOS 9.0, *) {
+            mapView.showsCompass = true
+            mapView.showsScale = true
+            mapView.showsTraffic = true
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - MKMapViewDelegate methods
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyPin"
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            return nil
+        }
+        
+        // Reuse the annotation if possible
+        var annotationView: MKAnnotationView?
+        
+        // This checks the available OS and executes the code accordingly. We will only execute the code in
+        // the if statement if the phone is running iOS 11 or up
+        if #available(iOS 11.0, *) {
+            var markerAnnotationView: MKMarkerAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+            
+            if markerAnnotationView == nil {
+                markerAnnotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                markerAnnotationView?.canShowCallout = true
+            }
+            
+            markerAnnotationView?.glyphText = "!"
+            markerAnnotationView?.markerTintColor = UIColor.systemTeal
+        
+            annotationView = markerAnnotationView
+            
+        } else {
+            
+            var pinAnnotationView: MKPinAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+            
+            if pinAnnotationView == nil {
+                pinAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                pinAnnotationView?.canShowCallout = true
+                pinAnnotationView?.pinTintColor = UIColor.orange
+            }
+            
+            annotationView = pinAnnotationView
+        }
+        
+        let leftIconView = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 53, height: 53))
+        leftIconView.image = UIImage(named: restaurant.image)
+        annotationView?.leftCalloutAccessoryView = leftIconView
+            
+        return annotationView
+    }
+    
+}
